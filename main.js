1b1657e453b8848742cca6780903af501849538c
edsveren <63107557+edsveren@users.noreply.github.com>
2020-08-29 19:50:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// the playground
+const app = new App();
+
+// for (let i = 0; i < 10; i++) {
+//     app.generator.generate();
+// }
+
+//app.run();
+
+//generator.drawCoins(canvas.context);
+
+// let coin = new Coin(3);
+// coin.draw(canvas.context);
+
+// coin = new Coin(2);
+// coin.x = 100;
+// coin.draw(canvas.context);
+
+// coin = new Coin(1);
+// coin.x = 200;
+// coin.draw(canvas.context);
